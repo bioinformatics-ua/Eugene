6bbc5624edcaf1c328f2c1f6ce377445a125ffb9
Eduardo Sousa <ed1000992@gmail.com>
2014-02-08 12:23:12 +0000

    export to TSV 50% done
    missing:
    -> Threading
    -> GUI

---
+++ src/pt/ua/ieeta/geneoptimizer/FileHandling/ExportTSV.java
@@ -0,0 +1,57 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package pt.ua.ieeta.geneoptimizer.FileHandling;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.text.DecimalFormat;
+import pt.ua.ieeta.geneoptimizer.geneDB.Gene;
+import pt.ua.ieeta.geneoptimizer.geneDB.Genome;
+import pt.ua.ieeta.geneoptimizer.geneDB.UsageAndContextTables;
+
+/**
+ *
+ * @author ed1000
+ */
+public class ExportTSV {
+    public static void exportToTSV (File tsvFile, Gene printingGene) {
+        try(PrintWriter tsvOut = new PrintWriter(tsvFile);){
+            Genome printingGenome = printingGene.getGenome();
+            UsageAndContextTables printingTables = printingGenome.getUsageAndContextTables();
+            
+            tsvOut.println("Genome name: " + "\t" + printingGenome.getName());
+            tsvOut.println("Gene name: " + "\t" + printingGene.getName());
+            tsvOut.println();
+            tsvOut.println("Number of Codons: " + "\t" + printingGene.getSequenceLength());
+            tsvOut.println("GC content: " + "\t" + new DecimalFormat("##.##").format(printingGene.getGCContent()*100));
+            tsvOut.println("Average RSCU: " + "\t" + new DecimalFormat("##.###").format(printingGene.getAverageRSCU()));
+            tsvOut.println("Codon Pair Bias: " + "\t" + new DecimalFormat("##.###").format(printingGene.getCPB()));
+            tsvOut.println("Effective Number of Codons: " + "\t" + new DecimalFormat("##.###").format(printingGene.getEffectiveNumberOfCodons()));
+            if(printingGene.hasCAI()) {
+                tsvOut.println("CAI: " + "\t" + new DecimalFormat("##.###").format(printingGene.getCAI()));
+            }
+            tsvOut.println();
+            
+            tsvOut.println("Index" + "\t" + "Codon" + "\t" + "Amino Acid" + "\t" + "Codon Pair Score"  + "\t" + "Codon Usage RSCU" + "\t" + "GC content" + (printingGene.hasCAI() ? "\t" + "Codon Usage (CAI)" : ""));
+            int idx = 1;
+            String actualCodon, actualAminoAcid, nextCodon;
+            for(int i = 0; i < printingGene.getSequenceLength(); i++) {
+                actualCodon = printingGene.getCodonAt(i);
+                actualAminoAcid = printingGenome.getAminoAcidFromCodon(actualCodon);
+                if(i != printingGene.getSequenceLength()-1) {
+                    nextCodon = printingGene.getCodonAt(i+1);
+                    tsvOut.println(idx++ + "\t" + actualCodon + "\t" + actualAminoAcid  + "\t" + printingTables.getCodonPairScore(actualCodon, nextCodon) + "\t" + printingTables.getCodonUsageRSCU(actualCodon) + "\t" + "GC content (todo)" + (printingGene.hasCAI() ? "\t" + printingGenome.getHouseKeepingGenes().getUsageAndContextTables().getCodonUsageRSCU(actualCodon) : ""));
+                } else {
+                    tsvOut.println(idx + "\t" + actualCodon + "\t" + "*" + "\t" + "\t" + "\t" + "\t" + printingTables.getCodonRelativeAdaptiveness(actualCodon) + "\t" + printingTables.getCodonUsageCount(actualCodon) + "\t" + printingTables.getCodonUsageFrequency(actualCodon) + "\t" + printingTables.getCodonUsageRSCU(actualCodon));
+                }
+            }
+        }catch(FileNotFoundException ex) {
+            
+        }
+    }
+}
